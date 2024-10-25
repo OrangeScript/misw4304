7d1f3d7b59930cd243b427a337287072bbae10b5
OrangeScript <da.naranjo10@uniandes.edu.co>
2024-10-25 10:49:52 -0500

    test cases para endpoint add_email

--- tests/commands/test_add_email.py
+++ tests/commands/test_add_email.py
@@ -16,7 +16,7 @@ class TestHealthCheck():
         request_bodies = []
         for i in range(5):
             body = {
-                'email': fake.ascii_company_email(),
+                'email': fake.ascii_email(),
                 'app_uuid': str(fake.uuid4()),
                 'blocked_reason': fake.pystr(),
                 'request_ip': fake.pystr()
@@ -29,5 +29,39 @@ class TestHealthCheck():
         # Test to ensure RouteCreation inherits from BaseCommand
         route = AddEmailToBlacklist(gen_request[0])
         assert isinstance(route, BaseCommand)
+
+    def test_invalid_email(self, gen_request):
+        # Test to ensure invalid email is caught
+        route = AddEmailToBlacklist(gen_request[0])
+        route.request_body['email'] = 'invalid_email'
+        response = route.execute()
+        assert response['status_code'] == 400
+        assert response['response']['msg'] == 'Invalid email'
+
+    def test_invalid_app_id(self, gen_request):
+        # Test to ensure invalid app id is caught
+        route = AddEmailToBlacklist(gen_request[1])
+        route.request_body['app_uuid'] = 'invalid_app_id'
+        response = route.execute()
+        assert response['status_code'] == 400
+        assert response['response']['msg'] == "Invalid App ID"
+
+    def test_email_in_blacklist(self, gen_request):
+        # Test to ensure email already in blacklist is caught
+        route = AddEmailToBlacklist(gen_request[2])
+        route.check_email_in_list = MagicMock(return_value=False)
+        response = route.execute()
+        assert response['status_code'] == 404
+        assert response['response']['msg'] == "Email already in blacklist"
+
+    def test_execute(self, gen_request):
+        # Test to ensure execute method works as expected
+        route = AddEmailToBlacklist(gen_request[3])
+        
+        with patch.object(BaseCommand, 'execute') as mock_execute:
+            mock_execute.return_value = {'response': 'Success', 'status_code': 200}
+            response = route.execute()
+            assert response['status_code'] == 201
+            assert response['response'] == f"New email {gen_request[3]['email']} added to blacklist"
 
     
