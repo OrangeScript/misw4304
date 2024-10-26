96067cfce05c2ad006db74f217d1363d37a5c22f
Juan Rodriguez <142239371+jprodriguez22@users.noreply.github.com>
2024-10-26 12:42:33 -0500

    Ajuste de archivo de tests para pruebas unitarias

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -8,8 +8,8 @@ PRODUCTION_ARGS = "production"
 DEVELOP_ENV = ".env.test"
 PRODUCTION_ENV = ".env.production"
 
-# Setup environment
-# if len(argv) > 1 and argv[1] == DEVELOP_ARGS:
-#     load_dotenv(DEVELOP_ENV)
+#Setup environment
+if len(argv) > 1 and argv[1] == DEVELOP_ARGS:
+    load_dotenv(DEVELOP_ENV)
 # if len(argv) > 1 and argv[1] == PRODUCTION_ARGS:
 #     load_dotenv(PRODUCTION_ENV)
